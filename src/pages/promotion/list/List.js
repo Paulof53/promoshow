f23af5721341c34f566eab6be760c64f035b10f1
paulof53 <paulo.freire53@gmail.com>
2020-07-12 22:22:49 -0300

    updating components

---
+++ src/pages/promotion/list/List.js
@@ -0,0 +1,27 @@
+import React from "react";
+import PromotionCard from "../Card/card";
+import "./List.css";
+
+const PromotionList = ({ loading, error, promotions }) => {
+  if (error) {
+    return <div>Algo não está certo</div>;
+  }
+
+  if (loading || promotions === null) {
+    return <div> Carregando... </div>;
+  }
+  if (promotions.length === 0) {
+    return <div>Nenhum resultado encontrado</div>;
+  }
+
+  return (
+    <div className="promotion-list">
+      {" "}
+      {promotions.map((promotion) => (
+        <PromotionCard promotion={promotion} />
+      ))}{" "}
+    </div>
+  );
+};
+
+export default PromotionList;
